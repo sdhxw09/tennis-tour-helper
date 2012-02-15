d10ab9bb24d5bdc1ab66513217d137c9f193be47
sdhxw09 <hwu@railinc.com>
2012-02-15 09:54:19 -0500

    create project tennis-tour-helper

---
+++ content.js
@@ -0,0 +1,26 @@
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    var option = request.option;
+    console.log('content script gets request ' + option);
+
+    if (option === "RANKING") {
+        var data = {};
+        var trElements = document.all.tags('tbody')[1].childNodes;
+        for (var i = 1; i < 201; i++) {
+            var ranking = trElements[i].firstElementChild.firstElementChild.firstChild.nodeValue;
+            var name = trElements[i].firstElementChild.nextSibling.firstElementChild.firstChild.nodeValue;
+            data[name.trim()] = ranking;
+        }
+
+        chrome.extension.sendRequest({'option': option, 'data': data});
+    }
+    else {
+        var data = [];
+        var aElements = $('#applicants a.cboxElement').each(function() {
+            var name = this.text;
+            data.push(name.trim());
+        });
+
+        chrome.extension.sendRequest({'option': option, 'data': data});
+    }
+});
+
