cd650c1fadae16ff2f14d8e799277527d53e73f1
U-RAILINC\sdhxw09 <sdhxw09@RAIL0131.railinc.com>
2012-04-15 19:29:20 -0400

    It works for NC now. Only 449 players on NC ranking list.

--- content.js
+++ content.js
@@ -4,14 +4,22 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 
     if (option === "RANKING") {
         var data = {};
-        var trElements = document.all.tags('tbody')[1].childNodes;
-        for (var i = 1; i < 501; i++) {
-            var ranking = trElements[i].firstElementChild.firstElementChild.firstChild.nodeValue;
-            var name = trElements[i].firstElementChild.nextSibling.firstElementChild.firstChild.nodeValue;
-            var state = trElements[i].firstElementChild.nextSibling.nextSibling.nextSibling.firstElementChild.firstChild.nodeValue;
 
-            var personalInfo = [ranking, state];
-            data[name.trim()] = personalInfo;
+        var numOfPlayers = document.getElementById('grdMain').getElementsByTagName('tbody')[0].childNodes.length - 1;
+        if (numOfPlayers > 500) {
+            numOfPlayers = 501;
+        }
+
+        var trElements = document.getElementById('grdMain').getElementsByTagName('tbody')[0].childNodes;
+        for (var i = 1; i < numOfPlayers; i++) {
+            if (typeof(trElements[i]) != 'undefined') {
+                var ranking = trElements[i].firstElementChild.firstElementChild.firstChild.nodeValue;
+                var name = trElements[i].firstElementChild.nextSibling.firstElementChild.firstChild.nodeValue;
+                var state = trElements[i].firstElementChild.nextSibling.nextSibling.nextSibling.firstElementChild.firstChild.nodeValue;
+
+                var personalInfo = [ranking, state];
+                data[name.trim()] = personalInfo;
+            }
         }
 
         chrome.extension.sendRequest({'option': option, 'data': data});
